03982913007e27bb50f86b24d4b46fc9b1b171e0
Victor_DSales <101744156+VictorSles@users.noreply.github.com>
2023-05-10 23:06:34 -0400

    .

--- intervalo de tempo 2/script.js
+++ intervalo de tempo 2/script.js
@@ -1,11 +1,17 @@
 function ativar() {
     tempo = setInterval(function () {
         var tempo = document.getElementById("divs").innerHTML;
-        var soma = parseInt(tempo) + 1;
-        document.getElementById("divs").innerHTML = soma;
-    }, 1000);
-}
+        var soma = parseInt(tempo) - 1;
 
-function parar(){
+        if (soma === -1) {
+            document.getElementById("divs").innerHTML = "Acabou"
+            parar()
+        } else {
+            document.getElementById("divs").innerHTML = soma;
+        }
+
+    },1000);
+}
+function parar() {
     clearInterval(tempo)
 }
