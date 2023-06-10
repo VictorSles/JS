2cf895b46c7591c711d1a3e380f660a108bbf6ac
Victor_DSales <101744156+VictorSles@users.noreply.github.com>
2023-06-09 23:20:36 -0400

    t

--- OBJETOS/objetos4.js
+++ OBJETOS/objetos4.js
@@ -1,9 +1,8 @@
-function enviar() {
-    let valorString = document.getElementById("texto").value;
-    let idiv = document.getElementById("idiv")
-    if (valorString % 2 == 0) {
-        idiv.innerHTML = `${valorString} é um numero PAR`;
-    } else {
-        idiv.innerHTML = `${valorString} é um numero IMPAR`;
+function enviar(){
+    let variavel = document.getElementById("texto").value;
+    let variavel_2 = Number(variavel);
+    while (variavel_2 < 1){
+        let variavel_3 = variavel_2 - 1
+        document.getElementById("idiv").innerHTML=`${variavel_3}`;
     }
 }
