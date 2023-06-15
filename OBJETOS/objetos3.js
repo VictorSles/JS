13bd4242607eca8fe3208e4d3b8c7c2434d1cb0c
Victor_DSales <101744156+VictorSles@users.noreply.github.com>
2023-06-14 22:27:26 -0400

    t

--- OBJETOS/objetos3.js
+++ OBJETOS/objetos3.js
@@ -1,9 +1,10 @@
-function enviar() {
-    let ajax = new XMLHttpRequest();
-    ajax.open("GET", "https://www.espruino.com/json/BANGLEJS.json");
-    ajax.send();
-    ajax.onload = function () {
-        document.getElementById("idiv2").innerHTML = this.responseText;
-    }
+let ajax = new XMLHttpRequest();
+ajax.open("GET", "https://www.espruino.com/json/BANGLEJS.json");
+ajax.send();
+ajax.onload = function () {
+    document.getElementById("idiv2").innerHTML = this.responseText;
+    let obj = JSON.parse(this.responseText);
+    console.log(obj);
 }
+
 
--- OBJETOS/objetos4.js
+++ OBJETOS/objetos4.js
@@ -1,4 +1,6 @@
 function enviar() {
     document.getElementById("idiv").innerHTML = `<iframe src="indexxxx.html" frameborder="0" style="width: 100%; height: 100%;"></iframe>`
+
 }
+
 
